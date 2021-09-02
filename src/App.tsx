505a6e9c959669f9645dcaac75d1e2f6a782e279
nicoulmete <nicolas.ulmete@mandiant.com>
2021-09-02 16:06:00 -0300

    fix 404 and remove gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         <Switch>
           <Route path={NOT_FOUND} exact component={NotFound} />
           <Route path={HOME} component={StatisticsContainer} />
-          {/* ...other routes... */}
+          {/* ...other routes here... */}
         </Switch>
       </BrowserRouter>
     </Container>
